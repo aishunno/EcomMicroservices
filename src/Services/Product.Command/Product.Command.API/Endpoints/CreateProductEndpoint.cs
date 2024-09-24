9992e194e503ebf5d257620606d14e47714b6bb8
Arif <ariful.islam@sme.additiv.com>
2024-09-24 13:23:04 +0600

    Minimal API basic setup with Carter

---
+++ src/Services/Product.Command/Product.Command.API/Endpoints/CreateProductEndpoint.cs
@@ -0,0 +1,14 @@
+﻿using Carter;
+
+namespace Product.Command.API.Endpoints;
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", () =>
+        {
+            return "Hello World";
+        });
+    }
+}
